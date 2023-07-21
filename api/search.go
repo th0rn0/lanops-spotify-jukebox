239c222d7e5ce12e7e230c6826ecf0a3df16d3c6
Th0rn0 <dev@th0rn0.co.uk>
2023-07-21 23:48:20 +0100

    cleanup

--- api/search.go
+++ api/search.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -10,36 +9,29 @@ import (
 )
 
 func handleSearch(c *gin.Context) {
+	var searchOutput SearchOutput
+
 	ctx := c.Request.Context()
 	searchTerm := c.Param("searchTerm")
-	fmt.Println(searchTerm)
 
 	results, err := client.Search(ctx, searchTerm, spotify.SearchTypeTrack)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(results.Tracks.Tracks[0].Album.Images)
-
-	searchOutput := SearchOutput{}
 
 	// handle artist results
 	if results.Artists != nil {
-		fmt.Println("Artists:")
 		for _, artist := range results.Artists.Artists {
 			artistInfo := ArtistSearchOutput{
 				Name: artist.Name,
 				ID:   artist.ID.String(),
 			}
 			searchOutput.Artists = append(searchOutput.Artists, artistInfo)
-			fmt.Println(artistInfo)
-			fmt.Println("   ", artist.Name)
 		}
 	}
 
 	// handle track results
 	if results.Tracks != nil {
-		fmt.Println("Tracks:")
 		for _, track := range results.Tracks.Tracks {
 			trackInfo := TrackSearchOutput{
 				Name:   track.Name,
@@ -48,8 +40,6 @@ func handleSearch(c *gin.Context) {
 				Images: track.Album.Images,
 			}
 			searchOutput.Tracks = append(searchOutput.Tracks, trackInfo)
-			fmt.Println(trackInfo)
-			fmt.Println("   ", track.Name)
 		}
 	}
 	c.JSON(http.StatusOK, searchOutput)
